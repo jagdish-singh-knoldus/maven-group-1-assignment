2cb28aba8c3d663d3250b60e4059ccb6e473c8b6
jagdish-singh-knoldus <76424088+jagdish-singh-knoldus@users.noreply.github.com>
2021-01-18 23:04:58 +0530

    Update Application.java

--- Group1/assign/cal-impl/src/main/java/com/knoldus/Application.java
+++ Group1/assign/cal-impl/src/main/java/com/knoldus/Application.java
@@ -7,7 +7,7 @@ public class Application {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter first number : ");
         int num1=sc.nextInt();
-        System.out.print("Enter first number : ");
+        System.out.print("Enter second number : ");
         int num2=sc.nextInt();
         System.out.println("Addition of "+num1+" and "+num2+" is "+cal.sum(num1,num2));
         System.out.println("Subtraction of "+num1+" from "+num2+" is "+cal.sub(num1,num2));
